dae4540cac49956dc3fd27cd9692dc1f7ead3da9
Jettapon Chuantis <jettapon.c@ayodiacompany.com>
2024-08-17 22:08:18 +0700

    fix

--- android/src/main/kotlin/com/ko2ic/imagedownloader/ImageDownloaderPlugin.kt
+++ android/src/main/kotlin/com/ko2ic/imagedownloader/ImageDownloaderPlugin.kt
@@ -322,8 +322,8 @@ class ImageDownloaderPlugin : FlutterPlugin, ActivityAware, MethodCallHandler {
                 when (it) {
                     is Downloader.DownloadStatus.Failed -> Log.d(LOGGER_TAG, it.reason)
                     is Downloader.DownloadStatus.Paused -> Log.d(LOGGER_TAG, it.reason)
-                    is Downloader.DownloadStatus.Successful -> Log.d(LOGGER_TAG, it.reason)
-                    is Downloader.DownloadStatus.Pending -> Log.d(LOGGER_TAG, it.reason)
+                    is Downloader.DownloadStatus.Successful -> Log.d(LOGGER_TAG, "Successful")
+                    is Downloader.DownloadStatus.Pending -> Log.d(LOGGER_TAG, "Pending")
                     is Downloader.DownloadStatus.Running -> {
                         Log.d(LOGGER_TAG, it.progress.toString())
                         val args = HashMap<String, Any>()
